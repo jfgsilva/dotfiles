16711d8c9c7a9c3e5ddb923bae2520c1c14e9dee
joaofilipesilva <joao.silva@clearops.com>
2024-05-28 10:51:30 +0100

    added prerequisites

--- setup.sh
+++ setup.sh
@@ -1,9 +1,22 @@
-# TODO: add neovim to installation using my forked repo
-#
 # This file should be executed just after cloning this repository
 
 # pre requisites
-PR="git make unzip gcc ripgrep"
+PR="git make unzip gcc ripgrep neovim"
+
+echo "Installing requirements"
+
+# Detect package manager and install prerequisites
+if command -v dnf >/dev/null 2>&1; then
+    sudo dnf install  "$PR fd-find"
+elif command -v apt-get >/dev/null 2>&1; then
+    sudo apt update
+    sudo apt install  "$PR xclip"
+elif command -v brew >/dev/null 2>&1; then
+    brew install $PR
+else
+    echo "No supported package manager found. Please install the prerequisites manually: $PR"
+    exit 1
+fi
 
 # Get the directory where the script is located
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
@@ -26,13 +39,17 @@ manage_repo() {
 # setup environment variables
 export XDG_CONFIG_HOME="$HOME"/.config
 
+echo "Creating directories"
 # Create directories
 mkdir -p "$XDG_CONFIG_HOME"/tmux
+mkdir -p "$XDG_CONFIG_HOME"/nvim/lua/custom/plugins
 
 
 
 # installing dependencies
 # tmux: don't forget to install plugins by running <prefix + I>
+echo "Setting up repositories"
+
 manage_repo "https://github.com/tmux-plugins/tpm" "$HOME/.tmux/plugins/tpm"
 tmux source "$XDG_CONFIG_HOME"/tmux/tmux.conf
 
@@ -41,6 +58,8 @@ manage_repo "https://github.com/jfgsilva/kickstart.nvim.git" "${XDG_CONFIG_HOME}
 
 
 # Symbolic links
+echo "Creating symlinks"
+
 ln -sf "$SCRIPT_DIR/tmux.conf" "$XDG_CONFIG_HOME"/tmux/tmux.conf
 ln -sf "$SCRIPT_DIR/nvim-plugins/filetree.lua" "$XDG_CONFIG_HOME"/nvim/lua/custom/plugins/filetree.lua
 ln -sf "$SCRIPT_DIR/nvim-plugins/init.lua" "$XDG_CONFIG_HOME"/nvim/lua/custom/plugins/init.lua
